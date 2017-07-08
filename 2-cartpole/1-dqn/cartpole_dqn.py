6f2b9cdc9747bcedc3ee7dd59215589a7ccd4431
dnddnjs <dnddnjs11@naver.com>
2017-07-08 11:36:03 +0900

    fix model saving part and make a2c code to be trainable

--- 2-cartpole/1-dqn/cartpole_dqn.py
+++ 2-cartpole/1-dqn/cartpole_dqn.py
@@ -14,7 +14,6 @@ EPISODES = 300
 # 카트폴 예제에서의 DQN 에이전트
 class DQNAgent:
     def __init__(self, state_size, action_size):
-        # 카트폴이 학습하는 모습을 보고 싶으면 self.render 을 True로 설정
         self.render = False
         self.load_model = False
 
@@ -22,7 +21,7 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
 
-        # DQN 하이퍼 파라미터
+        # DQN 하이퍼파라미터
         self.discount_factor = 0.99
         self.learning_rate = 0.001
         self.epsilon = 1.0
@@ -30,20 +29,21 @@ class DQNAgent:
         self.epsilon_min = 0.01
         self.batch_size = 64
         self.train_start = 1000
-        # 리플레이 메모리 , 최대 크기 2000
+
+        # 리플레이 메모리, 최대 크기 2000
         self.memory = deque(maxlen=2000)
 
-        # 모델과 타겟 모델 생성
+        # 모델과 타깃 모델 생성
         self.model = self.build_model()
         self.target_model = self.build_model()
 
-        # 타겟 모델 초기화
+        # 타깃 모델 초기화
         self.update_target_model()
 
         if self.load_model:
             self.model.load_weights("./save_model/cartpole_dqn_trained.h5")
 
-    # 상태가 입력 큐함수가 출력인 인공신경망 생성
+    # 상태가 입력, 큐함수가 출력인 인공신경망 생성
     def build_model(self):
         model = Sequential()
         model.add(Dense(24, input_dim=self.state_size, activation='relu',
@@ -56,7 +56,7 @@ class DQNAgent:
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
 
-    # 타겟 모델을 모델의 가중치로 업데이트
+    # 타깃 모델을 모델의 가중치로 업데이트
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
 
@@ -68,52 +68,49 @@ class DQNAgent:
             q_value = self.model.predict(state)
             return np.argmax(q_value[0])
 
-    # 샘플 <s,a,r,s'> 을 리플레이 메모리에 저장
+    # 샘플 <s, a, r, s'>을 리플레이 메모리에 저장
     def append_sample(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
 
     # 리플레이 메모리에서 무작위로 추출한 배치로 모델 학습
     def train_model(self):
-        if len(self.memory) < self.train_start:
-            return
-        # 메모리에서 배치크기만큼 무작위로 샘플 추출
-        batch_size = min(self.batch_size, len(self.memory))
-        mini_batch = random.sample(self.memory, batch_size)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
-        update_input = np.zeros((batch_size, self.state_size))
-        update_target = np.zeros((batch_size, self.state_size))
-        action, reward, done = [], [], []
+        # 메모리에서 배치 크기만큼 무작위로 샘플 추출
+        mini_batch = random.sample(self.memory, self.batch_size)
+
+        states = np.zeros((self.batch_size, self.state_size))
+        next_states = np.zeros((self.batch_size, self.state_size))
+        actions, rewards, dones = [], [], []
 
         for i in range(self.batch_size):
-            update_input[i] = mini_batch[i][0]
-            action.append(mini_batch[i][1])
-            reward.append(mini_batch[i][2])
-            update_target[i] = mini_batch[i][3]
-            done.append(mini_batch[i][4])
+            states[i] = mini_batch[i][0]
+            actions.append(mini_batch[i][1])
+            rewards.append(mini_batch[i][2])
+            next_states[i] = mini_batch[i][3]
+            dones.append(mini_batch[i][4])
 
         # 현재 상태에 대한 모델의 큐함수
-        # 다음 상태에 대한 타겟 모델의 큐함수
-        target = self.model.predict(update_input)
-        target_val = self.target_model.predict(update_target)
+        # 다음 상태에 대한 타깃 모델의 큐함수
+        target = self.model.predict(states)
+        target_val = self.target_model.predict(next_states)
 
-        # 벨만 최적 방정식을 이용한 타겟 업데이트
+        # 벨만 최적 방정식을 이용한 업데이트 타깃
         for i in range(self.batch_size):
-            if done[i]:
-                target[i][action[i]] = reward[i]
+            if dones[i]:
+                target[i][actions[i]] = rewards[i]
             else:
-                target[i][action[i]] = reward[i] + self.discount_factor * (
+                target[i][actions[i]] = rewards[i] + self.discount_factor * (
                     np.amax(target_val[i]))
 
-        self.model.fit(update_input, target, batch_size=self.batch_size,
+        self.model.fit(states, target, batch_size=self.batch_size,
                        epochs=1, verbose=0)
 
 
 if __name__ == "__main__":
     # CartPole-v1 환경, 최대 타임스텝 수가 500
     env = gym.make('CartPole-v1')
-    # 환경으로부터 상태와 행동의 크기 받아옴
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
 
@@ -133,26 +130,29 @@ if __name__ == "__main__":
             if agent.render:
                 env.render()
 
-            # get action for the current state and go one step in environment
+            # 현재 상태로 행동을 선택
             action = agent.get_action(state)
+            # 선택한 행동으로 환경에서 한 타임스텝 진행
             next_state, reward, done, info = env.step(action)
             next_state = np.reshape(next_state, [1, state_size])
-            # if an action make the episode end, then gives penalty of -100
+            # 에피소드가 중간에 끝나면 -100 보상
             reward = reward if not done or score == 499 else -100
 
-            # save the sample <s, a, r, s'> to the replay memory
+            # 리플레이 메모리에 샘플 <s, a, r, s'> 저장
             agent.append_sample(state, action, reward, next_state, done)
-            # every time step do the training
-            agent.train_model()
+            # 매 타임스텝마다 학습
+            if len(agent.memory) >= agent.train_start:
+                agent.train_model()
+
             score += reward
             state = next_state
 
             if done:
-                # every episode update the target model to be same with model
+                # 각 에피소드마다 타깃 모델을 모델의 가중치로 업데이트
                 agent.update_target_model()
 
-                # every episode, plot the play time
                 score = score if score == 500 else score + 100
+                # 에피소드마다 학습 결과 출력
                 scores.append(score)
                 episodes.append(e)
                 pylab.plot(episodes, scores, 'b')
@@ -160,11 +160,7 @@ if __name__ == "__main__":
                 print("episode:", e, "  score:", score, "  memory length:",
                       len(agent.memory), "  epsilon:", agent.epsilon)
 
-                # if the mean of scores of last 10 episode is bigger than 490
-                # stop training
+                # 이전 10개 에피소드의 점수 평균이 490보다 크면 학습 중단
                 if np.mean(scores[-min(10, len(scores)):]) > 490:
+                    agent.model.save_weights("./save_model/cartpole_dqn.h5")
                     sys.exit()
-
-        # save the model
-        if e % 50 == 0:
-            agent.model.save_weights("./save_model/cartpole_dqn_trained.h5")
--- 2-cartpole/2-actor-critic/cartpole_a2c.py
+++ 2-cartpole/2-actor-critic/cartpole_a2c.py
@@ -5,36 +5,37 @@ import numpy as np
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.optimizers import Adam
+from keras import backend as K
 
 EPISODES = 1000
 
 
-# A2C(Advantage Actor-Critic) agent for the Cartpole
+# 카트폴 예제에서의 액터-크리틱(A2C) 에이전트
 class A2CAgent:
     def __init__(self, state_size, action_size):
-        # if you want to see Cartpole learning, then change to True
         self.render = False
         self.load_model = False
-        # get size of state and action
+        # 상태와 행동의 크기 정의
         self.state_size = state_size
         self.action_size = action_size
         self.value_size = 1
 
-        # These are hyper parameters for the Policy Gradient
+        # 액터-크리틱 하이퍼파라미터
         self.discount_factor = 0.99
         self.actor_lr = 0.001
         self.critic_lr = 0.005
 
-        # create model for policy network
+        # 정책신경망과 가치신경망 생성
         self.actor = self.build_actor()
         self.critic = self.build_critic()
+        self.actor_updater = self.actor_optimizer()
+        self.critic_updater = self.critic_optimizer()
 
         if self.load_model:
             self.actor.load_weights("./save_model/cartpole_actor_trained.h5")
             self.critic.load_weights("./save_model/cartpole_critic_trained.h5")
 
-    # approximate policy and value using Neural Network
-    # actor: state is input and probability of each action is output of model
+    # actor: 상태를 받아 각 행동의 확률을 계산
     def build_actor(self):
         actor = Sequential()
         actor.add(Dense(24, input_dim=self.state_size, activation='relu',
@@ -42,54 +43,80 @@ class A2CAgent:
         actor.add(Dense(self.action_size, activation='softmax',
                         kernel_initializer='he_uniform'))
         actor.summary()
-        actor.compile(loss='categorical_crossentropy',
-                      optimizer=Adam(lr=self.critic_lr))
         return actor
 
-    # critic: state is input and value of state is output of model
+    # critic: 상태를 받아서 상태의 가치를 계산
     def build_critic(self):
         critic = Sequential()
+        critic.add(Dense(24, input_dim=self.state_size, activation='relu',
+                         kernel_initializer='he_uniform'))
         critic.add(Dense(24, input_dim=self.state_size, activation='relu',
                          kernel_initializer='he_uniform'))
         critic.add(Dense(self.value_size, activation='linear',
                          kernel_initializer='he_uniform'))
         critic.summary()
-        critic.compile(loss="mse", optimizer=Adam(lr=self.critic_lr))
         return critic
 
-    # using the output of policy network, pick action stochastically
+    # 정책신경망의 출력을 받아 확률적으로 행동을 선택
     def get_action(self, state):
         policy = self.actor.predict(state, batch_size=1).flatten()
         return np.random.choice(self.action_size, 1, p=policy)[0]
 
-    # update policy network every episode
-    def train_model(self, state, action, reward, next_state, done):
-        target = np.zeros((1, self.value_size))
-        advantages = np.zeros((1, self.action_size))
+    # 정책신경망을 업데이트하는 함수
+    def actor_optimizer(self):
+        action = K.placeholder(shape=[None, self.action_size])
+        advantage = K.placeholder(shape=[None, ])
 
+        action_prob = K.sum(action * self.actor.output, axis=1)
+        cross_entropy = K.log(action_prob) * advantage
+        loss = -K.sum(cross_entropy)
+
+        optimizer = Adam(lr=self.actor_lr)
+        updates = optimizer.get_updates(self.actor.trainable_weights, [], loss)
+        train = K.function([self.actor.input, action, advantage], [],
+                           updates=updates)
+        return train
+
+    # 가치신경망을 업데이트하는 함수
+    def critic_optimizer(self):
+        target = K.placeholder(shape=[None, ])
+
+        loss = K.mean(K.square(target - self.critic.output))
+
+        optimizer = Adam(lr=self.critic_lr)
+        updates = optimizer.get_updates(self.critic.trainable_weights, [], loss)
+        train = K.function([self.critic.input, target], [], updates=updates)
+
+        return train
+
+    # 각 타임스텝마다 정책신경망과 가치신경망을 업데이트
+    def train_model(self, state, action, reward, next_state, done):
         value = self.critic.predict(state)[0]
         next_value = self.critic.predict(next_state)[0]
 
-        if done:
-            advantages[0][action] = reward - value
-            target[0][0] = reward
-        else:
-            advantages[0][action] = reward + self.discount_factor * (
-                next_value - value)
-            target[0][0] = reward + self.discount_factor * next_value
+        act = np.zeros([1, self.action_size])
+        act[0][action] = 1
 
-        self.actor.fit(state, advantages, epochs=1, verbose=0)
-        self.critic.fit(state, target, epochs=1, verbose=0)
+        # 벨만 기대 방정식를 이용한 어드벤티지와 업데이트 타깃
+        if done:
+            advantage = reward - value
+            target = [reward]
+        else:
+            advantage = (reward + self.discount_factor * next_value) - value
+            target = reward + self.discount_factor * next_value
+
+        self.actor_updater([state, act, advantage])
+        self.critic_updater([state, target])
 
 
 if __name__ == "__main__":
-    # In case of CartPole-v1, maximum length of episode is 500
+    # CartPole-v1 환경, 최대 타임스텝 수가 500
     env = gym.make('CartPole-v1')
-    # get size of state and action from environment
+    # 환경으로부터 상태와 행동의 크기를 받아옴
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
 
-    # make A2C agent
+    # 액터-크리틱(A2C) 에이전트 생성
     agent = A2CAgent(state_size, action_size)
 
     scores, episodes = [], []
@@ -107,7 +134,7 @@ if __name__ == "__main__":
             action = agent.get_action(state)
             next_state, reward, done, info = env.step(action)
             next_state = np.reshape(next_state, [1, state_size])
-            # if an action make the episode end, then gives penalty of -100
+            # 에피소드가 중간에 끝나면 -100 보상
             reward = reward if not done or score == 499 else -100
 
             agent.train_model(state, action, reward, next_state, done)
@@ -116,7 +143,7 @@ if __name__ == "__main__":
             state = next_state
 
             if done:
-                # every episode, plot the play time
+                # 에피소드마다 학습 결과 출력
                 score = score if score == 500.0 else score + 100
                 scores.append(score)
                 episodes.append(e)
@@ -124,12 +151,8 @@ if __name__ == "__main__":
                 pylab.savefig("./save_graph/cartpole_a2c.png")
                 print("episode:", e, "  score:", score)
 
-                # if the mean of scores of last 10 episode is bigger than 490
-                # stop training
+                # 이전 10개 에피소드의 점수 평균이 490보다 크면 학습 중단
                 if np.mean(scores[-min(10, len(scores)):]) > 490:
+                    agent.actor.save_weights("./save_model/cartpole_actor.h5")
+                    agent.critic.save_weights("./save_model/cartpole_critic.h5")
                     sys.exit()
-
-        # save the model
-        if e % 50 == 0:
-            agent.actor.save_weights("./save_model/cartpole_actor_trained.h5")
-            agent.critic.save_weights("./save_model/cartpole_critic_trained.h5")
