18e3d14eb9ad4db2eca38c7b3c0970de6a064125
dnddnjs <dnddnjs11@naver.com>
2020-03-01 21:39:57 +0900

    policy iteration 코드 리뷰

--- 1-grid-world/1-policy-iteration/policy_iteration.py
+++ 1-grid-world/1-policy-iteration/policy_iteration.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-import random
+import numpy as np
 from environment import GraphicDisplay, Env
 
 
@@ -11,17 +10,17 @@ class PolicyIteration:
         self.value_table = [[0.0] * env.width for _ in range(env.height)]
         # 상 하 좌 우 동일한 확률로 정책 초기화
         self.policy_table = [[[0.25, 0.25, 0.25, 0.25]] * env.width
-                                    for _ in range(env.height)]
+                            for _ in range(env.height)]
         # 마침 상태의 설정
         self.policy_table[2][2] = []
-        # 감가율
+        # 할인율
         self.discount_factor = 0.9
 
+    # 벨만 기대 방정식을 통해 다음 가치함수를 계산하는 정책 평가
     def policy_evaluation(self):
-
         # 다음 가치함수 초기화
         next_value_table = [[0.00] * self.env.width
-                                    for _ in range(self.env.height)]
+                           for _ in range(self.env.height)]
 
         # 모든 상태에 대해서 벨만 기대방정식을 계산
         for state in self.env.get_all_states():
@@ -54,7 +53,7 @@ class PolicyIteration:
             # 반환할 정책 초기화
             result = [0.0, 0.0, 0.0, 0.0]
 
-            # 모든 행동에 대해서 [보상 + (감가율 * 다음 상태 가치함수)] 계산
+            # 모든 행동에 대해서 [보상 + (할인율 * 다음 상태 가치함수)] 계산
             for index, action in enumerate(self.env.possible_actions):
                 next_state = self.env.state_after_action(state, action)
                 reward = self.env.get_reward(state, action)
@@ -81,16 +80,10 @@ class PolicyIteration:
 
     # 특정 상태에서 정책에 따른 행동을 반환
     def get_action(self, state):
-        # 0 ~ 1 사이의 값을 무작위로 추출
-        random_pick = random.randrange(100) / 100
-
-        policy = self.get_policy(state)
-        policy_sum = 0.0
         # 정책에 담긴 행동 중에 무작위로 한 행동을 추출
-        for index, value in enumerate(policy):
-            policy_sum += value
-            if random_pick < policy_sum:
-                return index
+        policy = self.get_policy(state)
+        policy = np.array(policy)
+        return np.random.choice(4, 1, p=policy)[0]
 
     # 상태에 따른 정책 반환
     def get_policy(self, state):
@@ -100,7 +93,6 @@ class PolicyIteration:
 
     # 가치 함수의 값을 반환
     def get_value(self, state):
-        # 소숫점 둘째 자리까지만 계산
         return round(self.value_table[state[0]][state[1]], 2)
 
 if __name__ == "__main__":
