2a9e8a71b875ae18e605f6d607a591d23dc3f43d
hyeokreal <hyeokryeol.yang@gmail.com>
2020-03-14 02:01:58 +0800

    DQN 피드백 반영 test.py

--- 3-atari/1-breakout-dqn/test.py
+++ 3-atari/1-breakout-dqn/test.py
@@ -9,6 +9,7 @@ from skimage.transform import resize
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
 
 
+# 상태가 입력, 큐함수가 출력인 인공신경망 생성
 class DQN(tf.keras.Model):
     def __init__(self, action_size, state_size):
         super(DQN, self).__init__()
@@ -30,23 +31,28 @@ class DQN(tf.keras.Model):
         return q
 
 
+# 브레이크아웃 예제에서의 DQN 에이전트
 class DQNAgent:
     def __init__(self, action_size, state_size, model_path):
         self.render = False
 
+        # 상태와 행동의 크기 정의
         self.state_size = state_size
         self.action_size = action_size
+
         self.epsilon = 0.02
 
+        # 모델과 타깃 모델 생성
         self.model = DQN(action_size, state_size)
         self.model.load_weights(model_path)
 
+    # 입실론 탐욕 정책으로 행동 선택
     def get_action(self, history):
         history = np.float32(history / 255.0)
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
         else:
-            q_value = self.model.predict(history)
+            q_value = self.model(history)
             return np.argmax(q_value[0])
 
 
@@ -57,13 +63,17 @@ def pre_processing(observe):
 
 
 if __name__ == "__main__":
+    # 환경 세팅
     env = gym.make("BreakoutDeterministic-v4")
+    render = False
+
+    # 테스트를 위한 에이전트 생성
     state_size = (84, 84, 4)
     action_size = 3
     model_path = './save_model/trained/model'
-    render = False
-
     agent = DQNAgent(action_size, state_size, model_path)
+
+    # 불필요한 행동을 없애주기 위한 딕셔너리 선언
     action_dict = {0:1, 1:2, 2:3, 3:3}
 
     num_episode = 30
@@ -72,11 +82,14 @@ if __name__ == "__main__":
         dead = False
 
         score, start_life = 0, 5
+        # env 초기화
         observe = env.reset()
 
+        # 랜덤으로 뽑힌 값 만큼의 프레임동안 움직이지 않음
         for _ in range(random.randint(1, 30)):
             observe, _, _, _ = env.step(1)
 
+        # 프레임을 전처리 한 후 4개의 상태를 쌓아서 입력값으로 사용.
         state = pre_processing(observe)
         history = np.stack([state, state, state, state], axis=2)
         history = np.reshape([history], (1, 84, 84, 4))
@@ -85,15 +98,18 @@ if __name__ == "__main__":
             if render:
                 env.render()
 
+            # 바로 전 history를 입력으로 받아 행동을 선택
             action = agent.get_action(history)
-
+            # 1: 정지, 2: 왼쪽, 3: 오른쪽
             real_action = action_dict[action]
-
+            
+            # 죽었을 때 시작하기 위해 발사 행동을 함
             if dead:
                 action, real_action, dead = 0, 1, False
 
+            # 선택한 행동으로 환경에서 한 타임스텝 진행
             observe, reward, done, info = env.step(real_action)
-
+            # 각 타임스텝마다 상태 전처리
             next_state = pre_processing(observe)
             next_state = np.reshape([next_state], (1, 84, 84, 1))
             next_history = np.append(next_state, history[:, :, :, :3], axis=3)
@@ -111,4 +127,5 @@ if __name__ == "__main__":
                 history = next_history
 
             if done:
+                # 각 에피소드 당 테스트 정보를 기록
                 print("episode: {:3d} | score : {:4.1f}".format(e, score))
